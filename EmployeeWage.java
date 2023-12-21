653c972f0cc9a96eceecf179f5fdb76b85306927
RuchikaJamode <ruchikasjamode@gmail.com>
2023-12-21 18:45:07 +0530

    present

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,20 @@
 package com.bridgelabz.employeewage;
-
+import java.util.Random;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to Employee Wage computation program");
-
+		 int present=1;
+		Random random=new Random();
+		int randomno=random.nextInt(2);
+	
+		if(randomno==present) {
+			System.out.println("employee is present");
+			
+		}
+		else {
+		
+		System.out.println("employee is absent");
 	}
-
+	}
 }
